410bcc6eaa5bac3988509ecc1f4fdd9d3e262957
vmarcell96 <vajdam96@gmail.com>
2022-09-07 21:55:02 +0200

    created board component, implemeted  newGame method

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  squares!: any[];
+  xIsNext!: boolean;
+  winner!: string | null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  
+
+}
