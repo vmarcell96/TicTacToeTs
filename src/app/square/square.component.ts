270aef6afeeaa22d2d990d4e450350568f8a5869
vmarcell96 <vajdam96@gmail.com>
2022-09-07 21:14:41 +0200

    added square component

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      {{ rando }}
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+  rando = Math.random();
+
+}
