6c73c209a18f919815d77c8776d1948d6030d8ec
vmarcell96 <vajdam96@gmail.com>
2022-09-07 23:17:56 +0200

    ternary button variations added to template

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      {{ rando }}
-    </p>
+    <button *ngIf="!value">{{ value }}</button>
+    <button *ngIf="value == 'X'">{{ value }}</button>
+    <button *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: [
-  ]
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
-  rando = Math.random();
+  @Input() value!: 'X' | 'O' | null;
 
 }
