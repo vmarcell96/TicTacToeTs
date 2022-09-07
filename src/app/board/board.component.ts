1f581682f7ddea2f800e051c85c1dc21f755e2a5
vmarcell96 <vajdam96@gmail.com>
2022-09-07 23:18:16 +0200

    makeMove bugfix

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -12,7 +12,7 @@ export class BoardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.newGame();
   }
 
@@ -27,7 +27,7 @@ export class BoardComponent implements OnInit {
   }
 
   makeMove(idx: number) {
-    if (this.squares[idx]) {
+    if (!this.squares[idx]) {
       this.squares?.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext;
     }
